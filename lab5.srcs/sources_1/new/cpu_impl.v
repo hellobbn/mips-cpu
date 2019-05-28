4956a692c5b037797e98193021acedf509722d76
hellobbn <clfbbn@gmail.com>
2019-05-28 19:34:30 +0800

    [cpu] basic mips-pipelined CPU no hazard hardware for now. so it still does NOT work

--- lab5.srcs/sources_1/new/cpu_impl.v
+++ lab5.srcs/sources_1/new/cpu_impl.v
@@ -10,6 +10,13 @@
  * EX: reg_dst, alu_op, alu_src
  * M: branch, mem_read, mem_write
  * WB: reg_write, mem_to_reg
+ *
+ * =========== Instructions ===========
+ * j: PC = nPC; nPC = (PC & 0xf0000000) | (target << 2);
+ *
+ * ============ General Todo ===========
+ * TODO: Add J-Type Support
+ * TODO: Add I-Type Support
  */
 module cpu_impl(
     input               i_run,                  // Run signal
@@ -24,42 +31,27 @@ module cpu_impl(
 
     /* Wire from control unit */
     wire            w_id_reg_dst;
-    wire            w_id_reg_write;             // TODO: Where to write?
+    wire            w_id_reg_write;             
     wire            w_id_alu_op;
-    wire            w_alu_src_a;                // TODO: No longer needed
     wire            w_id_alu_src_b;
-    wire            w_ir_write;                 // TODO: No longer needed
     wire            w_id_mem_to_reg;
     wire            w_id_mem_write;                
     wire            w_id_mem_read;
-    wire            w_i_or_d;                   // TODO: No longer needed
-    wire            w_pc_write;                 // TODO: No longer needed
-    wire            w_pc_write_cond;            // TODO: No longer needed
     wire     [1:0]  w_id_pc_source;
     wire            w_id_branch;                         
 
     /* Wire from ALU Conteoller */
     wire     [2:0]  w_ex_alu_op_fc;         // Input to ALU, selecting OP
 
-    /* Wire from IR */
-    wire    [31:0]  w_instruction;          // IR output; TODO: Not needed
-
-    /* Wire from ALUOut */
-    wire    [31:0]  w_alu_out;              // ALUOut Register Output; TODO: Not Needed
-
     /* Wire From PC */
-    wire    [31:0]  w_if_pc;                   // PC output; TODO: not needed
+    wire    [31:0]  w_if_pc;                   // PC output;
 
     /* Wire from MUX */
     wire    [31:0]  w_mux_mem_addr;         // MUX from PC to Memory
     wire    [4:0]   w_ex_mux_ins_ir;        // MUX from IR to Registers
-    wire    [31:0]  w_mux_register_wdat;    // MUX deciding data to write to registers
+    wire    [31:0]  w_wb_mux_register_wdat; // MUX deciding data to write to registers
     wire    [31:0]  w_ex_mux_alu_in_b;      // MUX to ALU input B
     wire    [31:0]  w_if_mux_pc_wdat;       // MUX deciding data written to PC
-
-    /* Wire from Register */
-    wire    [31:0]   w_reg_a;               // A
-    wire    [31:0]   w_reg_b;               // B
 
     /* Wire from Memory */
     wire    [31:0]  w_if_in_from_mem;          // Instruction/data from memory
@@ -124,6 +116,16 @@ module cpu_impl(
     wire    [31:0]  w_mem_reg_file_rdat2;
     wire    [4:0]   w_mem_mux_ins_ir;
     wire            w_mem_pc_src;           // TODO: Add j
+    wire    [31:0]  w_mem_read_data;
+
+    /* Wire from MEM/WB */
+    wire    [69:0]  w_mem_wb;
+    wire            w_wb_reg_write;
+    wire            w_wb_mem_to_reg;
+    wire    [31:0]  w_wb_mem_read_data;
+    wire    [31:0]  w_wb_alu_out_result;
+    wire    [4:0]   w_wb_reg_addr;
+
 
     /* Wire for PC + 4 */
     wire    [31:0]  w_if_pc_plus_four;  
@@ -135,49 +137,37 @@ module cpu_impl(
     reg r_ex_zero, r_ex_b_pc_write;
 
     /* CPU Control */
-    // TODO: Change
     cpu_control Control(.i_run(i_run),
-                        .i_op(w_instruction[31:26]),
+                        .i_op(w_id_ins[31:26]),
                         .i_clk(i_clk),
                         .i_rst(i_rst),
+                        .i_ins(w_id_ins),
                         .o_reg_dst(w_id_reg_dst),
-                        .o_reg_write(w_reg_write),
+                        .o_reg_write(w_id_reg_write),
                         .o_alu_op(w_id_alu_op),
-                        .o_alu_src_a(w_alu_src_a),
                         .o_alu_src_b(w_id_alu_src_b),
-                        .o_ir_write(w_ir_write),
                         .o_mem_to_reg(w_id_mem_to_reg),
-                        .o_mem_write(w_mem_write),
+                        .o_mem_write(w_id_mem_write),
                         .o_mem_read(w_id_mem_read),
-                        .o_i_or_d(w_i_or_d),
-                        .o_pc_write(w_pc_write),
-                        .o_pc_write_cond(w_pc_write_cond),
-                        .o_pc_source(w_pc_source));
+                        .o_branch(w_id_branch));
 
     /* Instruction Memory */
-    // TODO: Del Write 
-    dist_mem_gen_0 Memory(.a(w_mux_mem_addr[9:2]),
+    // FIXME: now only using we === 0 to disable write
+    dist_mem_gen_0 Memory(.a(w_if_pc[9:2]),
                           .d(w_mem_reg_file_rdat2),
                           .dpra(i_addr),
                           .clk(i_clk),
-                          .we(w_mem_mem_write),
+                          .we(0),                       // never write
                           .spo(w_if_in_from_mem),
                           .dpo(o_mem_data));
-    
-    /* IR */
-    register IR(.clk(i_clk),
-                .rst(i_rst),
-                .i_dat(w_if_in_from_mem),
-                .i_we(w_ir_write),
-                .o_dat(w_instruction));
 
     /* Register File */
     register_file Registers(.i_read_addr_0(w_id_ins[25:21]),
                             .i_read_addr_1(w_id_ins[20:16]),
                             .i_read_addr_2(i_addr[4:0]),
-                            .i_write_addr(w_mux_ins_ir),
-                            .i_write_data(w_mux_register_wdat),
-                            .i_write_enable(w_reg_write),
+                            .i_write_addr(w_wb_reg_addr),
+                            .i_write_data(w_wb_mux_register_wdat),
+                            .i_write_enable(w_wb_reg_write),
                             .i_reset(i_rst),
                             .i_clk(i_clk),
                             .o_read_data_0(w_id_reg_file_rdat1),
@@ -189,28 +179,18 @@ module cpu_impl(
                  .in_b(w_ex_mux_alu_in_b),
                  .in_select(w_ex_alu_op_fc),
                  .out_flag(w_ex_alu_out_flag),
-                 .out_result(w_ex_alu_out_result)
-                 );
+                 .out_result(w_ex_alu_out_result));
     
     /* ALU Control */
     alu_control ALU_Control(.i_ins(w_ex_alu_ctrl_in),
                             .i_alu_op(w_ex_alu_op),
-                            .i_i_tp(w_instruction[31:26]),      // TODO: No longer needed
                             .o_alu_op(w_ex_alu_op_fc));
-
-    /* Memory data register */
-    // TODO: No longer needed
-    register Memory_Data_Register(.clk(i_clk),
-                                  .rst(i_rst),
-                                  .i_dat(w_if_in_from_mem),
-                                  .i_we(1),
-                                  .o_dat(w_mem_dat_reg));
     
     /* PC */
     register PC(.clk(i_clk),
                 .rst(i_rst),
-                .i_dat(w_mux_pc_wdat),
-                .i_we(w_if_pc_write_ctrl),
+                .i_dat(w_if_mux_pc_wdat),
+                .i_we(1),                           // Always write PC
                 .o_dat(w_if_pc));
 
     /* Sign Extend */
@@ -222,14 +202,9 @@ module cpu_impl(
                                .o_dat(w_ex_sl_two_32_bit));
 
     /* Shift Left 2 - in only 26 bits */
-    shift_left_two_26_to_28 Shift_Left_Two_26_BIT(.i_dat(w_instruction[25:0]),
-                            .o_dat(w_sl_two_26_bit));
-
-    /* MUX 1: Memory address */
-    two_way_mux MUX_MEM_ADDR(.i_zero_dat(w_pc),
-                             .i_one_dat(w_alu_out),
-                             .sel(w_i_or_d),
-                             .o_dat(w_mux_mem_addr));
+    // TODO: J-Type
+    // shift_left_two_26_to_28 Shift_Left_Two_26_BIT(.i_dat(w_instruction[25:0]),
+    //                         .o_dat(w_sl_two_26_bit));
 
     /* MUX 2: Register file Write register */
     two_way_mux_5_bit MUX_W_REG(.i_zero_dat(w_ex_insa),
@@ -238,13 +213,13 @@ module cpu_impl(
                                 .o_dat(w_ex_mux_ins_ir));
 
     /* MUX 3: Register file write data */
-    two_way_mux MUX_REG_FILE_WDAT(.i_zero_dat(w_alu_out),
-                                  .i_one_dat(w_mem_dat_reg),
-                                  .sel(w_mem_to_reg),
-                                  .o_dat(w_mux_register_wdat));
+    two_way_mux MUX_REG_FILE_WDAT(.i_zero_dat(w_wb_alu_out_result),
+                                  .i_one_dat(w_wb_mem_read_data),
+                                  .sel(w_wb_mem_to_reg),
+                                  .o_dat(w_wb_mux_register_wdat));
 
     /* MUX 4: deciding data written to PC */
-    // TODO: PC SRC
+    // TODO: J Type
     four_way_mux MUX_DAT_TO_PC(.i_zero_dat(w_if_pc_plus_four),
                                .i_one_dat(w_mem_b_addr),
                                .i_two_dat({w_if_pc[31:28], w_sl_two_26_bit}),       // TODO: THIS NEEDS TO BE CHAMGED
@@ -257,31 +232,13 @@ module cpu_impl(
                              .i_third_dat(w_ex_sl_two_32_bit),
                              .i_sel(w_ex_alu_src_b),
                              .o_dat(w_ex_mux_alu_in_b));
-
-    /* Register A */
-    // TODO: No longer needed
-    register REG_A(.clk(i_clk),
-                   .rst(i_rst),
-                   .i_dat(w_reg_file_rdat1),
-                   .i_we(1),
-                   .o_dat(w_reg_a));
-
-    /* Register B */
-    // TODO: No longer needed
-    register REG_B(.clk(i_clk),
-                   .rst(i_rst),
-                   .i_dat(w_reg_file_rdat2),
-                   .i_we(1),
-                   .o_dat(w_reg_b));
-
-    /* ALU Out */
-    register ALUOut(.clk(i_clk),
-                    .rst(i_rst),
-                    .i_dat(w_alu_out_result),
-                    .i_we(1),
-                    .o_dat(w_alu_out));
     
-    /* TODO: Data memory */
+    /* Data memory */
+    dist_mem_gen_1 Data_Memory(.a(w_mem_b_addr),
+                               .d(w_mem_reg_file_rdat2),
+                               .clk(i_clk),
+                               .we(w_mem_mem_write),
+                               .spo(w_mem_read_data));
 
     /* IF/ID Register - 64 bit*/
     register_if_id IF_ID(.clk(i_clk),
@@ -335,7 +292,19 @@ module cpu_impl(
     assign w_mem_reg_file_rdat2 =   w_ex_mem[36:5];
     assign w_mem_mux_ins_ir =       w_ex_mem[4:0];
     
-    /* MEM/WB Register */
+    /* MEM/WB Register - 70 bit*/
+    register_mem_wb MEM_WB(.clk(i_clk),
+                           .rst(i_rst),
+                           .i_dat({w_mem_mux_ins_ir, w_mem_reg_write, w_mem_mem_to_reg, w_mem_read_data, w_mem_alu_out_result}),
+                           .i_we(1),
+                           .o_dat(w_mem_wb));
+
+    assign w_wb_reg_addr = w_mem_wb[69:66];
+    assign w_wb_reg_write = w_mem_wb[65];
+    assign w_wb_mem_to_reg = w_mem_wb[64];
+    assign w_wb_mem_read_data = w_mem_wb[63:32];
+    assign w_wb_alu_out_result = w_mem_wb[31:0];
+    
 
     /* Zero Branch Check */
     always @(*) begin
@@ -360,7 +329,7 @@ module cpu_impl(
     assign o_pc_data = w_if_pc[9:2];                   // 8 Bit output
 
     /* PC + 4 */
-    assign w_if_pc_plus_four = w_pc + 4;
+    assign w_if_pc_plus_four = w_if_pc + 4;
 
     /* EX B Addr */
     assign w_ex_b_addr = w_ex_pc_plus_four + w_ex_sl_two_32_bit;
